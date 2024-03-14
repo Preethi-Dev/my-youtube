2acc8b6000bd46f2305d95bbc730cc87b4aa2044
Preethi <selvampreethi360@gmail.com>
2024-03-14 11:19:12 +0530

    setup youtube api, fetch and store data from api in store

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const YOUTUBE_API_KEY = "AIzaSyCwIIAj6FYJeNQ3lAlTGq8zHWf3gPh-rQ4";
+export const YOUTUBE_API_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  YOUTUBE_API_KEY;
