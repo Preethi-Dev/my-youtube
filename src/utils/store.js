e3a5b20396aaf2b699bc26fef5cf52b705eff468
Preethi <selvampreethi360@gmail.com>
2024-03-14 11:15:44 +0530

    create store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
+import moviesReducer from "./moviesSlice";
 
 const store = configureStore({
   reducer: {
     app: appReducer,
+    movies: moviesReducer,
   },
 });
 
