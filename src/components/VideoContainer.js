10e6669c810c538898f344a8cc6df1e560f9b57e
Preethi <selvampreethi360@gmail.com>
2024-03-14 11:13:41 +0530

    create VideoContainer component

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { YOUTUBE_API_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addPopularMovies } from "../utils/moviesSlice";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const dispatch = useDispatch();
+
+  const popularMovies = useSelector((store) => store.movies.popularMovies);
+
+  const fetchPopularVideos = async () => {
+    const data = await fetch(YOUTUBE_API_URL);
+    const json = await data.json();
+    console.log(json.items);
+    dispatch(addPopularMovies(json.items));
+  };
+
+  useEffect(() => {
+    if (!popularMovies) fetchPopularVideos();
+  }, []);
+
+  //early return
+  if (!popularMovies) return;
+
+  return (
+    <div className="flex flex-wrap gap-2 justify-center">
+      {popularMovies.map((popularMovie) => (
+        <Link to={"/watch?v=" + popularMovie.id} key={popularMovie.id}>
+          <VideoCard movie={popularMovie} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default VideoContainer;
