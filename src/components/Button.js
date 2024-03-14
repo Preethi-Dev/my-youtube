cb85ab569241b3ec452d8eb50a0dd9c91be1c914
Preethi <selvampreethi360@gmail.com>
2024-03-14 11:14:44 +0530

    create ButtonList feature

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ name }) => {
+  return <button className="px-4 py-2 rounded-lg bg-gray-300">{name}</button>;
+};
+
+export default Button;
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  const categories = [
+    "music",
+    "News",
+    "Live",
+    "Podcast",
+    "JavaScript",
+    "Public Speaking",
+    "Pop Music",
+    "Tamil Cinema",
+    "Motiavtion",
+    "Programming",
+  ];
+  return (
+    <div className="flex gap-3 m-5 justify-center">
+      {categories.map((category) => (
+        <Button name={category} key={category} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
