1e4899466399b9559bef11e073b4d7854b972e1b
Preethi <selvampreethi360@gmail.com>
2024-03-22 11:33:11 +0530

    add youtube search suggestion api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,6 @@ const YOUTUBE_API_KEY = "AIzaSyCwIIAj6FYJeNQ3lAlTGq8zHWf3gPh-rQ4";
 export const YOUTUBE_API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
+
+export const YOUTUBE_SEARCH_SUGGESTION_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
