04d11ac3cc3c754b901e6cf63ed8b1aad5bcea63
preethi <selvampreethi360@gmail.com>
2024-03-25 22:13:09 +0530

    add API links

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,18 @@ export const YOUTUBE_API_URL =
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_SEARCH_VIDOE_LIST_API =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=" +
+  YOUTUBE_API_KEY +
+  "&q=";
+
+export const YOUTUBE_CHANNEL_BY_ID =
+  "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  YOUTUBE_API_KEY +
+  "&id=";
+
+export const YOUTUBE_COMMENTS_BY_VIDEO_ID =
+  "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key=" +
+  YOUTUBE_API_KEY +
+  "&videoId=";
