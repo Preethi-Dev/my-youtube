2d90bc4d0c1139766b64ffd9e67bd89e679c9146
preethi <selvampreethi360@gmail.com>
2024-03-27 17:11:19 +0530

    provide chatSlice to store

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
 import moviesReducer from "./moviesSlice";
 import searchReducer from "./searchSlice";
+import chatReducer from "./chatSlice";
 
 const store = configureStore({
   reducer: {
     app: appReducer,
     movies: moviesReducer,
     search: searchReducer,
+    chat: chatReducer,
   },
 });
 
