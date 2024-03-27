ebe5c2d30e9944ebcfb80e67d7d7a103f5ceaaf3
preethi <selvampreethi360@gmail.com>
2024-03-27 17:14:14 +0530

    Add LiveChat component to WatchPage

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -5,6 +5,7 @@ import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
 import CommentsList from "./CommentsList";
 import { YOUTUBE_COMMENTS_BY_VIDEO_ID } from "../utils/constants";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const isMenuOpen = useSelector((Store) => Store.app.isMenuOpen);
@@ -26,18 +27,25 @@ const WatchPage = () => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <div className={classnames("mt-24", { "ml-48": isMenuOpen })}>
-        <iframe
-          className="p-3"
-          width="1200"
-          height="600"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div className="flex">
+          <div>
+            <iframe
+              className="p-3"
+              width="1400"
+              height="720"
+              src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+
+          <LiveChat />
+        </div>
+
         {commentsData && (
           <div className="m-2 p-2">
             <h3 className="font-bold text-xl">{`Comments (${commentsData.length})`}</h3>
